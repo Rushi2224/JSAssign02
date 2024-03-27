78e6b78f373f576854a51ccd3e388ed1abc9fc59
Rushi2224 <137185381+Rushi2224@users.noreply.github.com>
2024-03-26 20:55:33 -0400

    Add files via upload

---
+++ JS_Assign2/JS/script.js
@@ -0,0 +1,50 @@
+function createSmoothie() {
+  const flavor = document.getElementById("smoothieFlavor").value;
+  const base = document.getElementById("smoothieBase").value;
+  const toppings = [...document.getElementById("smoothieToppings").selectedOptions].map(option => option.value);
+  const smoothie = new Smoothie(flavor, base, toppings);
+  displaySmoothie(smoothie);
+}
+
+class Smoothie {
+  constructor(flavor, base, toppings) {
+      this.flavor = flavor;
+      this.base = base;
+      this.toppings = toppings;
+  }
+  calculatePrice() {
+      let totalPrice = 0;
+      const flavorOption = document.querySelector(`#smoothieFlavor option[value='${this.flavor}']`);
+      const baseOption = document.querySelector(`#smoothieBase option[value='${this.base}']`);
+      const toppingsOptions = this.toppings.map(topping => document.querySelector(`#smoothieToppings option[value='${topping}']`));
+    
+      if (flavorOption && baseOption && toppingsOptions.every(option => option)) {
+          totalPrice += parseFloat(flavorOption.getAttribute('data-price')) || 0;
+          totalPrice += parseFloat(baseOption.getAttribute('data-price')) || 0;
+          toppingsOptions.forEach(option => {
+              totalPrice += parseFloat(option.getAttribute('data-price')) || 0;
+          });
+      } else {
+          console.error("Error: Some HTML elements not found or null values in smoothie properties.");
+      }
+      return totalPrice.toFixed(2);
+  }
+}
+
+function displaySmoothie(smoothie) {
+  const smoothieOutput = document.getElementById("smoothieOutput");
+  smoothieOutput.innerHTML = `
+      <h2>Your Smoothie:</h2>
+      <p>Flavor: ${smoothie.flavor} - $${parseFloat(document.querySelector(`#smoothieFlavor option[value='${smoothie.flavor}']`).getAttribute('data-price')).toFixed(2)}</p>
+      <p>Base: ${smoothie.base} - $${parseFloat(document.querySelector(`#smoothieBase option[value='${smoothie.base}']`).getAttribute('data-price')).toFixed(2)}</p>
+      <p>Toppings: ${smoothie.toppings.map(topping => `${topping} - $${parseFloat(document.querySelector(`#smoothieToppings option[value='${topping}']`).getAttribute('data-price')).toFixed(2)}`).join(', ')}</p>
+  `;
+  const bill = document.getElementById("bill");
+  const price = smoothie.calculatePrice();
+  bill.innerHTML = `
+      <h2>Bill:</h2>
+      <p>Total Price: $${price}</p>
+  `;
+  const smoothieImage = document.getElementById("smoothieImage");
+  smoothieImage.innerHTML = `<img src="Images/image.webp" alt="Smoothie" width="200">`; 
+}
